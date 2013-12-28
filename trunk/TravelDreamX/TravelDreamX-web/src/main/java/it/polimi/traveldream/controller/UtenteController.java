926430e5f01c58a4aae21ae208f7ea5ea0383321
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2013-12-28 09:05:27 +0000

    import del progetto web

---
+++ trunk/TravelDreamX/TravelDreamX-web/src/main/java/it/polimi/traveldream/controller/UtenteController.java
@@ -0,0 +1,35 @@
+package it.polimi.traveldream.controller;
+
+
+import it.polimi.traveldream.model.Utente;
+import it.polimi.traveldream.service.UtenteServiceLocal;
+import javax.ejb.EJB;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author rh0x
+ */
+@Controller
+public class UtenteController {
+   
+    @EJB(mappedName = "java:global/TravelDreamX-ear/TravelDreamX-web-1.0-SNAPSHOT/UtenteService")
+    private UtenteServiceLocal bean;
+
+    public void setBean(UtenteServiceLocal bean) {
+        this.bean = bean;
+    }
+    
+    
+    
+    @RequestMapping(value = "registrazione")
+    public @ResponseBody Utente registrazione() {
+        Utente utente = new Utente("leo", "culo");
+        utente.setStato("abilitato");
+        return bean.registrazione( utente);
+    }    
+
+    
+}
