886f2aaacdf523b1514f3053655db15c7642f3fc
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-05 09:54:35 +0000

    tentativo di far funzionare gli unit test

---
+++ branches/testAlpha/TravelDreamX/TravelDreamX-ejb/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/model/Visita.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.polimi.traveldream.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ *
+ * @author Dario
+ */
+@Entity
+@DiscriminatorValue("2")
+@Table(name = "visite")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Visita.findAll", query = "SELECT v FROM Visita v"),
+    @NamedQuery(name = "Visita.findByIdVisita", query = "SELECT v FROM Visita v WHERE v.idVisita = :idVisita"),
+    @NamedQuery(name = "Visita.findByDataOra", query = "SELECT v FROM Visita v WHERE v.dataOra = :dataOra"),
+    @NamedQuery(name = "Visita.findByNumeroPersone", query = "SELECT v FROM Visita v WHERE v.numeroPersone = :numeroPersone"),
+    @NamedQuery(name = "Visita.findByCosto", query = "SELECT v FROM Visita v WHERE v.costo = :costo")})
+public class Visita extends Voce implements Serializable {
+    private static final long serialVersionUID = 1L;
+    //@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idVisita")
+    private Integer idVisita;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dataOra")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataOra;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "numeroPersone")
+    private int numeroPersone;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "costo")
+    private float costo;
+    @JoinColumn(name = "idMuseo", referencedColumnName = "idMuseo")
+    @ManyToOne(optional = false)
+    private Museo idMuseo;
+
+    public Visita() {
+    }
+
+    public Visita(Integer idVisita) {
+        this.idVisita = idVisita;
+    }
+
+    public Visita(Integer idVisita, Date dataOra, int numeroPersone, float costo) {
+        this.idVisita = idVisita;
+        this.dataOra = dataOra;
+        this.numeroPersone = numeroPersone;
+        this.costo = costo;
+    }
+
+    public Integer getIdVisita() {
+        return idVisita;
+    }
+
+    public void setIdVisita(Integer idVisita) {
+        this.idVisita = idVisita;
+    }
+
+    public Date getDataOra() {
+        return dataOra;
+    }
+
+    public void setDataOra(Date dataOra) {
+        this.dataOra = dataOra;
+    }
+
+    public int getNumeroPersone() {
+        return numeroPersone;
+    }
+
+    public void setNumeroPersone(int numeroPersone) {
+        this.numeroPersone = numeroPersone;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    public Museo getIdMuseo() {
+        return idMuseo;
+    }
+
+    public void setIdMuseo(Museo idMuseo) {
+        this.idMuseo = idMuseo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idVisita != null ? idVisita.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Visita)) {
+            return false;
+        }
+        Visita other = (Visita) object;
+        if ((this.idVisita == null && other.idVisita != null) || (this.idVisita != null && !this.idVisita.equals(other.idVisita))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.polimi.traveldream.model.Visita[ idVisita=" + idVisita + " ]";
+    }
+    
+}
