e2d627087d26079f5c84b5b7d840d28d53c3165e
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-06 10:44:59 +0000

    creazione branch per integrare angular nel progetto

---
+++ branches/angularjsAlpha/TravelDreamX/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/model/Utente.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.polimi.traveldream.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.enterprise.inject.Default;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author rh0x
+ */
+@Entity
+@Table(name = "utenti")
+@NamedQueries({
+    @NamedQuery(name = "Utente.findAll", query = "SELECT u FROM Utente u"),
+    @NamedQuery(name = "Utente.findByEmail", query = "SELECT u FROM Utente u WHERE u.email = :email"),
+    @NamedQuery(name = "Utente.findByPassword", query = "SELECT u FROM Utente u WHERE u.password = :password"),
+    @NamedQuery(name = "Utente.findByAbilitato", query = "SELECT u FROM Utente u WHERE u.abilitato = :abilitato"),
+    @NamedQuery(name = "Utente.findByLivello", query = "SELECT u FROM Utente u WHERE u.livello = :livello")})
+public class Utente implements Serializable {
+    private static final long serialVersionUID = 1L;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Column(name = "password")
+    private String password;
+    @Column(name = "abilitato")
+    private Boolean abilitato;
+    @Column(name = "livello")
+    private Integer livello;
+
+    public Utente() {
+    }
+
+    public Utente(String email) {
+        this.email = email;
+    }
+
+    public Utente(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAbilitato() {
+        return abilitato;
+    }
+
+    public void setAbilitato(Boolean abilitato) {
+        this.abilitato = abilitato;
+    }
+
+    public Integer getLivello() {
+        return livello;
+    }
+
+    public void setLivello(Integer livello) {
+        this.livello = livello;
+    }
+
+   
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Utente)) {
+            return false;
+        }
+        Utente other = (Utente) object;
+        if ((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.polimi.traveldream.model.prova.Utente[ email=" + email + " ]";
+    }
+    
+}
