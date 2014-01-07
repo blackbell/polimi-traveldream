b4ce4b4a9a0943981ffaa0da2d081e91dfefaf80
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-07 15:21:55 +0000

    sistemata l'inizializzazione dell'EJBContainer, aggiunte le directory necessarie per l'embedded glassfish, creata suite per i test per i servizi, creato qualche test per i servizi utente (UtenteServiceTest.java), ripristinati i test sui DAO Rotta e Albergo

--- TravelDreamX/TravelDreamX-ejb/src/test/java/it/polimi/traveldream/service/EJBServiceTest.java
+++ TravelDreamX/TravelDreamX-ejb/src/test/java/it/polimi/traveldream/service/EJBServiceTest.java
@@ -6,60 +6,40 @@
 
 package it.polimi.traveldream.service;
 
-import it.polimi.traveldream.model.Utente;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import javax.ejb.embeddable.EJBContainer;
-import javax.naming.NamingException;
-import org.glassfish.internal.embedded.Server;
+import static it.polimi.traveldream.service.EJBServiceTestSuite.container;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 
 /**
  *
  * @author Dario
  */
 public class EJBServiceTest {
+   
+    @BeforeClass
+    public static void setUp(){
+        EJBServiceTestSuite.setUp();
+    }
+    @AfterClass
+    public static void tearDown(){
+        EJBServiceTestSuite.tearDown();
+    }
+
+    @Test
+    public void testEmbeddedContainerLoading(){
+        assertNotNull("Cannot retrieve container.context", container.getContext());
+    }
     
-   @Test
-   @Ignore
-   public void testGlassfishLoading(){
-       System.out.println("======== testGlassfishLoading ==============");
-        Server.Builder builder = new Server.Builder("TravelDreamX.testServer");
-        Server server = builder.build();
-        assertNotNull("Cannot instantiate GF server!", server);
-   }
-   
-   @Test
-   @Ignore
-   public void testEmbeddedContainerLoading() throws InterruptedException{
-       System.out.println("======== testEmbeddedContainerLoading ==============");
-       Map<String, Object> myMap = new HashMap<String, Object>();
-       myMap.put("org.glassfish.ejb.embedded.glassfish.installation.root", "./src/test/glassfish");
-       myMap.put(EJBContainer.MODULES, new File("target/classes/"));
-       EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer(myMap);
-       assertNotNull("Cannot instantiate EJB containter", container);
-       assertNotNull("Cannot retrieve container.context", container.getContext());
-       container.close();
-   }
-   
-   @Test
-   //@Ignore
-   public void testEmbeddedContainerLoadingService() throws NamingException{
-       System.out.println("======== testEmbeddedContainerLoadingService ==============");
-       Map<String, Object> myMap = new HashMap<String, Object>();
-       myMap.put("org.glassfish.ejb.embedded.glassfish.installation.root", "./src/test/glassfish");
-       myMap.put(EJBContainer.MODULES, new File("target/classes"));
-       EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer(myMap);
-       assertNotNull("Cannot instantiate EJB containter!", container);
-       UtenteServiceLocal instance = (UtenteServiceLocal)container.getContext().lookup("java:global/classes/UtenteService");
-       assertNotNull("Cannot retrieve service class!", instance);
-       System.out.println("---------------- invoking service ----------------");
-       Utente utente = new Utente("testUser", "testPsw");
-       Utente result = instance.registrazione(utente);
-       assertEquals(utente, result);
-       container.close();
-   }
+    /*
+    @Test
+    @Ignore
+    public void testGlassfishLoading(){
+        System.out.println("======== testGlassfishLoading ==============");
+         Server.Builder builder = new Server.Builder("TravelDreamX.testServer");
+         Server server = builder.build();
+         assertNotNull("Cannot instantiate GF server!", server);
+    }
+    */
 }
