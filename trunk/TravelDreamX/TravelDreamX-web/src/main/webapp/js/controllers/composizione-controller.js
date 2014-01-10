9e2865f7b3fbb7eb6dc8ef3dfbaecbb7caea640b
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-10 16:48:45 +0000

    merge con angularjsBeta (rev.94).

--- trunk/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/composizione-controller.js
+++ trunk/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/composizione-controller.js
@@ -1,44 +1,68 @@
 'use strict';
 travelDreamApp.controller('composizioneController', function($scope, $rootScope) {
-    $scope.getPVdaRootScope = function(){
+    $scope.getPVdaRootScope = function() {
         $scope.PV = $rootScope.PV;
-        if(typeof $scope.PV === 'undefined'){
+        if (typeof $scope.PV === 'undefined') {
             inizializzaPV();
-        };
+        }
+        ;
     };
-    var inizializzaPV = function (){
+    var inizializzaPV = function() {
         $scope.PV = {
             composizioneCollection: [
-                { 
-                    voce:{
+                {
+                    voce: {
                         tipo: 'volo',
                         costo: 1000.99,
                         dataOra: '22-feb-2014 10:25',
-                        rotta: { 
+                        rotta: {
                             cittaPartenza: 'Milano',
                             cittaArrivo: 'Amsterdam'
                         }
                     }
-                }, 
-                {   voce:{
+                },
+                {voce: {
                         tipo: 'soggiorno'
                     }
-                }, 
-                {   voce:{
+                },
+                {voce: {
                         tipo: 'visita'
                     }
                 }
             ]
         };
     };
-    $scope.isScelto = function (voce){
-        console.log(voce.tipo + " " + (typeof voce.costo !== 'undefined') + " costo " + voce.costo);
+    $scope.isSelezionato = function (tipo){
+        return $scope.selezionato === tipo;
+    };
+    $scope.seleziona = function (tipo){
+        $scope.selezionato = tipo;
+    };
+    
+    $scope.isScelto = function(voce) {
+        //console.log(voce.tipo + " " + (typeof voce.costo !== 'undefined') + " costo " + voce.costo);
         return (typeof voce.costo !== 'undefined');
     };
-    $scope.isVolo = function (voce){
+    $scope.isVolo = function(voce) {
         return (($scope.isScelto(voce)) && (voce.tipo === 'volo'));
     };
-    
-    
-    
+
+    $scope.eliminaVoce = function(indice) {
+        $scope.selezionato = 'nonSelezionato';
+        if (indice > -1) {
+            $scope.PV.composizioneCollection.splice(indice, 1);
+        }
+    };
+    $scope.creaVoce = function(tipo) {
+        var composizione = {
+            voce: {
+                tipo: tipo
+            }
+        };
+
+        $scope.PV.composizioneCollection.push(composizione);
+        console.log($scope.PV.composizioneCollection);
+    };
+
+
 });
