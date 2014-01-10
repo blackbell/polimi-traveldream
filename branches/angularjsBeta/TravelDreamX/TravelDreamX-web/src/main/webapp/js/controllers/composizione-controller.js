132031776a9be59669c6819a23ab0ba3066891c9
leonardo.rossetti5@gmail.com <leonardo.rossetti5@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-10 12:54:13 +0000

    Riconosce se è stata richiesta la modifica di un PV o la creazione di PV

--- branches/angularjsBeta/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/composizione-controller.js
+++ branches/angularjsBeta/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/composizione-controller.js
@@ -1,11 +1,23 @@
 'use strict';
 travelDreamApp.controller('composizioneController', function($scope, $rootScope) {
+    $scope.getPVdaRootScope = function(){
+        $scope.PV = $rootScope.PV;
+        if(typeof $scope.PV === 'undefined'){
+            inizializzaPV();
+        };
+    };
     var inizializzaPV = function (){
         $scope.PV = {
             composizioneCollection: [
                 { 
                     voce:{
-                        tipo: 'volo'
+                        tipo: 'volo',
+                        costo: 1000.99,
+                        dataOra: '22-feb-2014 10:25',
+                        rotta: { 
+                            cittaPartenza: 'Milano',
+                            cittaArrivo: 'Amsterdam'
+                        }
                     }
                 }, 
                 {   voce:{
@@ -19,12 +31,14 @@ travelDreamApp.controller('composizioneController', function($scope, $rootScope)
             ]
         };
     };
-    
-    $scope.getPVdaRootScope = function(){
-        $scope.PV = $rootScope.PV;
-        if(typeof $scope.PV === 'undefined'){
-            inizializzaPV();
-        };
+    $scope.isScelto = function (voce){
+        console.log(voce.tipo + " " + (typeof voce.costo !== 'undefined') + " costo " + voce.costo);
+        return (typeof voce.costo !== 'undefined');
     };
+    $scope.isVolo = function (voce){
+        return (($scope.isScelto(voce)) && (voce.tipo === 'volo'));
+    };
+    
+    
     
 });
