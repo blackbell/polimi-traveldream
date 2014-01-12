1ea60f76b0c169ab83b8c6c1d978503c61523a38
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-12 12:08:18 +0000

    implementato in maniera farlocca il servizio di recupero di un PB in base ai parametri di ricerca (per Leo).

--- trunk/TravelDreamX/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/service/PBService.java
+++ trunk/TravelDreamX/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/service/PBService.java
@@ -6,8 +6,12 @@
 package it.polimi.traveldream.service;
 
 import it.polimi.traveldream.data.VoceDAO;
+import it.polimi.traveldream.model.Rotta;
 import it.polimi.traveldream.model.Voce;
 import it.polimi.traveldream.model.Volo;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,8 +39,32 @@ public class PBService implements PBServiceLocal{
     }
 
     @Override
-    public Voce trovaPB(ParametriRicercaPB params) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<Voce> trovaPB(ParametriRicercaPB params) {
+        List<Voce> ret = null;
+        switch(params.getTipo()){
+            case Volo:
+                ret = new ArrayList<>();
+                Rotta rotta = new Rotta();
+                rotta.setAeroportoPartenza("San Giorgio International Airport");
+                rotta.setAeroportoArrivo("Malpensa");
+                rotta.setNazionePartenza("Italia");
+                rotta.setNazioneArrivo("Italia");
+                rotta.setCompagniaAerea("PoliMI airways");
+                rotta.setCittaPartenza("Pistoia");
+                rotta.setCittaArrivo("Milano");
+                Volo volo = new Volo();
+                volo.setDataOra(new Date());
+                volo.setRotta(rotta);
+                volo.setNumPasseggeri(3);
+                volo.setCosto(120.7f);
+                volo.setAbilitato(true);
+                ret.add(volo);
+                break;
+            default:
+                break;
+        }
+        return ret;
+        
     }
     
     
--- trunk/TravelDreamX/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/service/ParametriRicercaPB.java
+++ trunk/TravelDreamX/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/service/ParametriRicercaPB.java
@@ -5,10 +5,59 @@
 
 package it.polimi.traveldream.service;
 
+import it.polimi.traveldream.model.TipoPB;
+import java.util.Date;
+
 /**
  *
  * @author Dario
  */
 public class ParametriRicercaPB {
+    private TipoPB tipo;
+    private Integer idRotta;
+    private Integer idAlbergo;
+    private Integer idMuseo;
+    private Date data;
+
+    public TipoPB getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPB tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getIdRotta() {
+        return idRotta;
+    }
+
+    public void setIdRotta(Integer idRotta) {
+        this.idRotta = idRotta;
+    }
+
+    public Integer getIdAlbergo() {
+        return idAlbergo;
+    }
+
+    public void setIdAlbergo(Integer idAlbergo) {
+        this.idAlbergo = idAlbergo;
+    }
+
+    public Integer getIdMuseo() {
+        return idMuseo;
+    }
+
+    public void setIdMuseo(Integer idMuseo) {
+        this.idMuseo = idMuseo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
     
 }
