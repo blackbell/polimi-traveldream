41753d14983338d44c5d7270f154eaa4eb0a290c
leonardo.rossetti5@gmail.com <leonardo.rossetti5@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-14 12:53:43 +0000

    Sistemate varie cose grafiche

--- branches/angularjsBeta/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/autenticazione-controller.js
+++ branches/angularjsBeta/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/autenticazione-controller.js
@@ -1,66 +1,71 @@
 'use strict';
 
 travelDreamApp.controller('autenticazioneController', function($scope, $location, registrazioneService, loginService) {
-
+    toastr.options = {
+        positionClass: "toast-center"
+    };
     $scope.waiting = false;
-    
+
     $scope.registrazione = function(utente, form) {
-        if(form.$valid){
+        if (form.$valid) {
             $scope.waiting = true;
-            registrazioneService.registrazione(utente,function(esito){
+            registrazioneService.registrazione(utente, function(esito) {
                 console.log("Esito registrazione: " + JSON.stringify(esito));
-                if(esito.result){
+                if (esito.result) {
                     //$location.path(result.newUrl);
-                    toastr.success("l'utente " + utente.email + " puo' ora loggarsi.","Registrazione avvenuta con successo");
+                    toastr.success("l'utente " + utente.email + " puo' ora loggarsi.", "Registrazione avvenuta con successo");
                     $scope.dismiss();
-                }else
-                    toastr.error("l'utente " + utente.email + " e' gia' presente.","Registrazione fallita");
+                } else
+                    toastr.error("l'utente " + utente.email + " e' gia' presente.", "Registrazione fallita");
                 $scope.waiting = false;
             });
         } else {
-            toastr.error("Compila tutti i campi richiesti nel modo corretto.","ERRORE");
+            toastr.error("Compila tutti i campi richiesti nel modo corretto.", "ERRORE");
         }
     };
-    
+
     $scope.login = function(utente, form) {
-        if(form.$valid){
+        if (form.$valid) {
             $scope.waiting = true;
-            loginService.login(utente,function(esito){
+            loginService.login(utente, function(esito) {
                 console.log("Esito login: " + JSON.stringify(esito));
-                if(esito.result){
+                if (esito.result) {
                     //$location.path(result.newUrl);
                     //$scope.dismiss();
                     $scope.utente = esito.returnedObj;
-                    toastr.success("l'utente " + utente.email + " è ora loggato","Login avvenuto con successo");
-                }else
-                    toastr.error("Email " + utente.email + " o password " + utente.password + " errati.","Login fallito");
-                $scope.waiting = false ;
+                    $scope.dismiss();
+                    toastr.success("l'utente " + utente.email + " è ora loggato", "Login avvenuto con successo");
+                } else
+                    toastr.error("Email " + utente.email + " o password " + utente.password + " errati.", "Login fallito");
+                $scope.waiting = false;
             });
         } else {
-            toastr.error("Compila tutti i campi richiesti nel modo corretto.","ERRORE");
+            toastr.error("Compila tutti i campi richiesti nel modo corretto.", "ERRORE");
         }
     };
-   
-    $scope.logout = function (){
-        delete( $scope.utente );
+
+    $scope.logout = function() {
+        delete($scope.utente);
         toastr.success("Logout effettuato. DA SISTEMARE", "Arrivederci!");
     };
-    
-    $scope.checkEmail = function(formEmail){
-        if(formEmail.$valid){
-            toastr.success("Il campo è stato compilato correttamente","OK");
+
+    $scope.checkEmail = function(formEmail) {
+        if (formEmail.$valid) {
+            toastr.success("Il campo è stato compilato correttamente", "OK");
         } else {
-            toastr.warning("Inserisci un indirizzo valido (es. maro.rossi@jmail.com)","Attenzione:");
-        };
+            toastr.warning("Inserisci un indirizzo valido (es. maro.rossi@jmail.com)", "Attenzione:");
+        }
+        ;
     };
-    $scope.checkPwd = function(formPwd){
-        if(formPwd.$valid){
-            toastr.success("Il campo è stato compilato correttamente","OK");
+    $scope.checkPwd = function(formPwd) {
+        if (formPwd.$valid) {
+            toastr.success("Il campo è stato compilato correttamente", "OK");
         } else {
-            toastr.warning("La password deve avere lunghezza compresa tra 5 e 15 caratteri","Attenzione:");
-        };
+            toastr.warning("La password deve avere lunghezza compresa tra 5 e 15 caratteri", "Attenzione:");
+        }
+        ;
     };
-    $scope.isLogged = function (){
+    $scope.isLogged = function() {
         return typeof $scope.utente !== 'undefined';
     };
 });
