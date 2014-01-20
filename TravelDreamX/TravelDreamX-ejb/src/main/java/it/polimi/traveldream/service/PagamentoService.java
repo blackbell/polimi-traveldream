8512b57697ac6343987886ed97fd4c0dc56dfcb6
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-20 22:43:47 +0000

    definito l'ejb per i servizi di pagamento di pv e pb

---
+++ TravelDreamX/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/service/PagamentoService.java
@@ -0,0 +1,38 @@
+/*
+ * Politecnico di Milano, Software Engineering 2 (autumn semester)
+ * proj codename: TravelDreamX
+ */
+
+package it.polimi.traveldream.service;
+
+import it.polimi.traveldream.model.Pacchetto;
+import it.polimi.traveldream.model.Pagamento;
+import it.polimi.traveldream.model.Utente;
+import it.polimi.traveldream.model.Voce;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Dario
+ */
+@Stateless
+public class PagamentoService implements PagamentoServiceLocal {
+
+    @Override
+    public Pagamento pagamentoPV(Pacchetto pv, Utente u) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Pagamento pagamentoPB(Voce pb, Utente u) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Pagamento> recuperaPagamenti(Date da, Date a, Utente u, Pacchetto pv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
