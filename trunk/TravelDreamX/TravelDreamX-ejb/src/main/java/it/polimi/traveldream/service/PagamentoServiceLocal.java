59422ac3b7db2722b8aec5a1aaf2a37c416eefd3
dario.nesi@gmail.com <dario.nesi@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-20 22:43:47 +0000

    definito l'ejb per i servizi di pagamento di pv e pb

---
+++ trunk/TravelDreamX/TravelDreamX-ejb/src/main/java/it/polimi/traveldream/service/PagamentoServiceLocal.java
@@ -0,0 +1,28 @@
+/*
+ * Politecnico di Milano, Software Engineering 2 (autumn semester)
+ * proj codename: TravelDreamX
+ */
+
+package it.polimi.traveldream.service;
+
+import it.polimi.traveldream.model.Pacchetto;
+import it.polimi.traveldream.model.Pagamento;
+import it.polimi.traveldream.model.Utente;
+import it.polimi.traveldream.model.Voce;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Dario
+ */
+@Local
+public interface PagamentoServiceLocal {
+    
+    public Pagamento pagamentoPV(Pacchetto pv, Utente u);
+    public Pagamento pagamentoPB(Voce pb, Utente u);
+    
+    public List<Pagamento> recuperaPagamenti(Date da, Date a, Utente u, Pacchetto pv);
+    
+}
