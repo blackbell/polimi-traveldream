04bd9ebc39c2f4c2c07b9eb1b104994e20979770
leonardo.rossetti5@gmail.com <leonardo.rossetti5@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-22 10:39:13 +0000

    [FIX] le proprietà del parametriRicercaPB non utilizzate ai fini della ricerca vengono ora eliminate prima di effettuare la richiesta a trovaPB.json [NEW] aggiunta pagina Amministrazione

--- TravelDreamX/TravelDreamX-web/src/main/webapp/js/services/amministrazione-service.js
+++ TravelDreamX/TravelDreamX-web/src/main/webapp/js/services/amministrazione-service.js
@@ -5,4 +5,39 @@
 'use strict';
 
 travelDreamApp.factory('amministrazioneService', function($http) {
+
+    var __getUtenti = function(parametriRicercaUtenti, callback) {
+        $http({method: 'GET', data: parametriRicercaUtenti, url: 'getUtenti.json'}).
+                success(function(data, status, headers, config) {
+                    callback(data);
+                }).
+                error(function(data, status, headers, config) {
+                    toastr.error("Errore " + status);
+                });
+    };
+    var __disattivaUtente = function(utente, callback) {
+        $http({method: 'UPDATE', data: utente, url: 'disattivaUtente.json'}).
+                success(function(data, status, headers, config) {
+                    callback(data);
+                }).
+                error(function(data, status, headers, config) {
+                    toastr.error("Errore " + status);
+                });
+    };
+    var __recuperaPagamenti = function(parametriRicercaPagamenti, callback) {
+        $http({method: 'GET', data: parametriRicercaPagamenti, url: 'recuperaPagamenti.json'}).
+                success(function(data, status, headers, config) {
+                    callback(data);
+                }).
+                error(function(data, status, headers, config) {
+                    toastr.error("Errore " + status);
+                });
+    };
+
+
+    return {
+        getUtenti: __getUtenti,
+        disattivaUtente: __disattivaUtente,
+        recuperaPagamenti: __recuperaPagamenti
+    };
 });
