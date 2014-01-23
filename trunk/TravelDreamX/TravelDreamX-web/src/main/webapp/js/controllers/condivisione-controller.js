65d7f2cfe3aa0c5ea8687d328163bb4fc2579e23
leonardo.rossetti5@gmail.com <leonardo.rossetti5@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-23 18:34:20 +0000

    varie modifiche

--- trunk/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/condivisione-controller.js
+++ trunk/TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/condivisione-controller.js
@@ -4,9 +4,27 @@
  */
 'use strict';
 travelDreamApp.controller('condivisioneController', function($scope, $rootScope) {
+    toastr.options = {
+        positionClass: "toast-center"
+    };
+    
+    $scope.emails = new Array(new Object());
     $scope.getLinkCondivisione = function() {
         if (typeof $rootScope.linkCondivisione !== 'undefined')
             $scope.linkCondivisione = $rootScope.linkCondivisione;
+    };
+    $scope.aggiungiCampoEmail = function() {
+        $scope.emails.push(new Object());
+    };
+    $scope.condividi = function (){
+        toastr.success("il tuo pacchetto è stato condiviso!", "Successo:");
+        $scope.dismiss();
+    };
+    $scope.ceAlmenoUnIndirizzo = function (){
+        if($scope.emails[0].indirizzoEmail.length>6)
+            return true;
+        else
+            return false;
     };
 });
 
