9c3486f5061ef9a10c85b878f38dc5239fc4adf4
leonardo.rossetti5@gmail.com <leonardo.rossetti5@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-01-31 09:55:00 +0000

    Sistemato AcquistaPV

--- TravelDreamX/TravelDreamX-web/src/main/webapp/js/services/acquista-service.js
+++ TravelDreamX/TravelDreamX-web/src/main/webapp/js/services/acquista-service.js
@@ -6,10 +6,21 @@
 'use strict';
 
 travelDreamApp.factory('acquistaService', function($http) {
-    var eliminaProprietàNullePagamento = function (p){
-        
+    var eliminaProprietàNullePV = function (p){
+        if(!p.cittaAlbergo)
+            delete p.cittaAlbergo;
+        if(!p.giornoFine)
+            delete p.giornoFine;
+        if(!p.giornoInizio)
+            delete p.giornoInizio;
+        if(!p.nazioneArrivo)
+            delete p.nazioneArrivo;
+        if(!p.nazionePartenza)
+            delete p.nazionePartenza;
+        return p;
     };
     var __acquistaPV = function(pv, callback){
+        pv = eliminaProprietàNullePV(pv);
         $http({method: 'POST', data:pv, url: 'acquistaPV.json'}).
                 success(function (data, status, headers, config){
                     callback(data);
