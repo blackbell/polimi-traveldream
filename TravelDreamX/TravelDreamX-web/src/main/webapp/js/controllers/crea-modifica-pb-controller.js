91b2a78dfad06e840ce2bc4548f921313bbe5c69
leonardo.rossetti5@gmail.com <leonardo.rossetti5@gmail.com@63fd9a39-ffda-f7ec-ba98-d4c63111a67e>
2014-02-24 12:44:53 +0000

    [FIX] validazione in crea-ModificaPB funzionante

--- TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/crea-modifica-pb-controller.js
+++ TravelDreamX/TravelDreamX-web/src/main/webapp/js/controllers/crea-modifica-pb-controller.js
@@ -23,35 +23,75 @@ travelDreamApp.controller('creaModificaPBController', function($scope, $rootScop
                 $scope.PBdaSalvare['albergo'] = $scope.EDB;
             if ($scope.PBdaSalvare.tipo === 'Visita')
                 $scope.PBdaSalvare['museo'] = $scope.EDB;
-            
+
             delete $rootScope.tipoPBdaCreare;
             delete $rootScope.EDBperPB;
-        }else{
-            $scope.PBdaSalvare=$rootScope.PBdaModif;
-            if($scope.PBdaSalvare.tipo === 'Volo')
+        } else {
+            $scope.PBdaSalvare = $rootScope.PBdaModif;
+            if ($scope.PBdaSalvare.tipo === 'Volo')
                 $scope.EDB = $scope.PBdaSalvare.rotta;
-            if($scope.PBdaSalvare.tipo === 'Soggiorno')
+            if ($scope.PBdaSalvare.tipo === 'Soggiorno')
                 $scope.EDB = $scope.PBdaSalvare.albergo;
-            if($scope.PBdaSalvare.tipo === 'Visita')
+            if ($scope.PBdaSalvare.tipo === 'Visita')
                 $scope.EDB = $scope.PBdaSalvare.museo;
             delete $rootScope.PBdaModif;
         }
     };
-    $scope.salvaPB = function() {
-        if (true) {
-        gestioneOffertaService.salvaPB($scope.PBdaSalvare, function(esito) {
-            if (esito.result) {
-                toastr.success("pb salvato correttamente", esito.message);
-            } else
-                toastr.error(esito.message, "ERRORE:");
-            delete $rootScope.tipoPBdaCreare;
-            delete $rootScope.EDBperPB;
-            $scope.dismiss();
-            console.log("EDBperPB eliminato? " + $rootScope.EDBperPB);
-        });
-    }else{
-        toastr.warning("Compila tutti i campi correttamente");
+    var isComplete = function(pb) {
+        if (pb.tipo === 'Volo') {
+            if (!pb.dataOra){
+                toastr.warning("Inserire una data");
+                return false;
+            };
+            if (!pb.costo){
+                toastr.warning("Inserire un costo");
+                return false;
+            };
+        };
+        if (pb.tipo === 'Visita') {
+            if (!pb.dataOra){
+                toastr.warning("Inserire una data");
+                return false;
+            };
+            if (!pb.costo){
+                toastr.warning("Inserire un costo");
+                return false;
+            };
+        };
+        if (pb.tipo === 'Soggiorno') {
+            if (!pb.giornoInizio){
+                toastr.warning("Inserire una data d'inizio soggiorno");
+                return false;
+            };
+            if (!pb.giornoInizio){
+                toastr.warning("Inserire una data di fine soggiorno");
+                return false;
+            };
+            if (!pb.numeroPersone){
+                toastr.warning("Inserire il numero di persone");
+                return false;
+            };
+            if (!pb.costo){
+                toastr.warning("Inserire un costo");
+                return false;
+            };
+        };
+        
+        return true;
     };
+    $scope.salvaPB = function() {
+        if (isComplete($scope.PBdaSalvare)) {
+            gestioneOffertaService.salvaPB($scope.PBdaSalvare, function(esito) {
+                if (esito.result) {
+                    toastr.success("pb salvato correttamente", esito.message);
+                } else
+                    toastr.error(esito.message, "ERRORE:");
+                delete $rootScope.tipoPBdaCreare;
+                delete $rootScope.EDBperPB;
+                $scope.dismiss();
+                console.log("EDBperPB eliminato? " + $rootScope.EDBperPB);
+            });
+        };
     };
 });
 
